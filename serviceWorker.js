44f1d1e7e1b7f72ff06371e32e8e46d8c9cb1bec
benis developer <benissalondebelleza@gmail.com>
2026-09-18 10:06:29 -0500

    Updates

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,15 +1,17 @@
 const CACHE_NAME = 'benis-citas-v1';
 const FILES_TO_CACHE = [
-  '/',
-  '/index.html',
-  '/assets/index-CjQbvGe5.js',
-  '/assets/index-DHK8ogAd.css',
+  '/agendarcitas/',
+  '/agendarcitas/index.html',
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(FILES_TO_CACHE);
+      return cache.addAll(FILES_TO_CACHE).catch(() => {
+        return Promise.allSettled(
+          FILES_TO_CACHE.map((url) => cache.add(url).catch(() => {}))
+        );
+      });
     })
   );
 });
@@ -18,11 +20,7 @@ self.addEventListener('activate', (event) => {
   event.waitUntil(
     caches.keys().then((cacheNames) => {
       return Promise.all(
-        cacheNames.map((name) => {
-          if (name !== CACHE_NAME) {
-            return caches.delete(name);
-          }
-        })
+        cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
       );
     })
   );
@@ -31,7 +29,9 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        return caches.match('/agendarcitas/index.html');
+      });
     })
   );
 });
