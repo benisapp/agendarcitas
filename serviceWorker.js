b54927ee64272b0a879b198db9161f051a7f6274
benis developer <benissalondebelleza@gmail.com>
2026-09-18 09:44:24 -0500

    Updates

---
+++ serviceWorker.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'benis-citas-v1';
+const FILES_TO_CACHE = [
+  '/',
+  '/index.html',
+  '/assets/index-CjQbvGe5.js',
+  '/assets/index-DHK8ogAd.css',
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      );
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
